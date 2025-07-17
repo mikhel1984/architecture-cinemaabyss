19da64c5ca75d4eb29b35c049e356d94fd75b60e
Stanislav <mikhel.sk@gmail.com>
2025-07-17 22:11:11 +0300

    Prepare event microservice

---
+++ src/microservices/events/go.mod
@@ -0,0 +1,5 @@
+module github.com/cinemaabyss/microservices/proxy
+
+go 1.23
+
+require github.com/confluentinc/confluent-kafka-go/v2/kafka v1
---
+++ src/microservices/events/main.go
@@ -0,0 +1,95 @@
+import (
+	"log"
+	"net/http"
+	"encoding/json"
+	//"time"
+    //"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+type Event struct {
+	ID		int		`json:"id"`
+	Type	string	`json:"type"`
+	Timestamp	string	`json:"timestamp"`
+	Payload	[]string	`json:"payload"`
+}
+
+type Movie struct {
+	Status 	string	`json:"status"`
+	Partition	int	`json:"partition"`
+	Offset	int		`json:"offset"`
+	Event	Event	`json:"event"`
+}
+
+func main() {
+
+	http.HandleFunc("/api/events/health", handleHealth)
+	http.HandleFunc("/api/events/movie", handleMovies)
+	http.HandleFunc("/api/events/user", handleUser)
+	http.HandleFunc("/api/events/payment", handlePayment)
+
+	// producer, err := kafka.NewProducer(&kafka.ConfigMap{
+	// 	"bootstrap.servers": "host1:9092,host2:9092",
+	// })	
+	// if err != nil {
+	// 	log.Println(err)
+	// 	return
+	// }
+	// defer producer.Close()
+	
+	// consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+	// 	"bootstrap.servers":    "host1:9092,host2:9092",
+	// })
+	// if err != nil {
+	// 	log.Println(err)
+	// 	return
+	// }
+	// defer consumer.Close()
+	
+	
+}
+
+func handleHealth(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]bool{"status": true})
+}
+
+func handleMovies(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+		createMovie(w, r)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func handleUser(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+		createMovie(w, r)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func handlePayment(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+		createMovie(w, r)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	var m Movie
+	m.Status = "success"
+	m.Partition = 0
+	m.Offset = 42
+	m.Event.ID = "movie-1-viewd"
+	m.Event.Type = "movie"
+	m.Event.Timestamp = "2023-01-15T14:30:00Z"
+	json.NewEncoder(w).Encode(m)
+}
